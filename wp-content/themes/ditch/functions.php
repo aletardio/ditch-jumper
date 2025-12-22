1de2d0abb20cfaff01afcfa5148ffc67e85a82ab
aletardio <ale.tardio99@gmail.com>
2025-12-22 11:24:54 +0100

    Revert "add header"
    This reverts commit 75a09f891e0d7c0e40fbb6042e77aec03b99cad3.

--- wp-content/themes/ditch/functions.php
+++ wp-content/themes/ditch/functions.php
@@ -1,19 +1,10 @@
 <?php
 function ditch_assets() {
   wp_enqueue_style(
-    'ditch-jumper-style',
-    get_stylesheet_directory_uri() . '/assets/css/style.css',
+    'miosito-style',
+    get_stylesheet_uri(),
     array(),
     wp_get_theme()->get('Version')
   );
 }
 add_action('wp_enqueue_scripts', 'ditch_assets');
-
-<?php
-function ditch_setup() {
-  register_nav_menus([
-    'main-menu' => __('Menu principale', 'ditch-jumper'),
-  ]);
-}
-add_action('after_setup_theme', 'ditch_setup');
-
