e33c7d86a76bb89b0c8bf0fac2487181cee9aa74
aletardio <ale.tardio99@gmail.com>
2025-12-29 14:48:42 +0100

    layout section 5

--- wp-content/themes/ditch/front-page.php
+++ wp-content/themes/ditch/front-page.php
@@ -212,8 +212,33 @@ get_header();
     <section class="section-5">
         <div class="container-faq">
             <h2 class="heading-xl black text-center">
-                <?php echo get_field('titolo_faq') ?: 'Domande frequenti'; ?>
+                <?php echo get_field('titolo_sezione_5') ?: 'Domande frequenti'; ?>
             </h2>
+
+            <div class="col-2"></div>
+            <div class="col-8">
+                <div class="faq-accordion">
+                    <?php if(have_rows('faq')): ?>
+                        <?php while(have_rows('faq')): the_row(); 
+                            $domanda = get_sub_field('domanda');
+                            $risposta = get_sub_field('risposta');
+                        ?>
+                            <div class="faq-item">
+                                <button class="faq-question" aria-expanded="false">
+                                    <span class="faq-question-text"><?php echo esc_html($domanda); ?></span>
+                                    <span class="faq-icon">+</span>
+                                </button>
+                                <div class="faq-answer" aria-hidden="true">
+                                    <div class="faq-answer-content">
+                                        <?php echo wp_kses_post($risposta); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="col-2"></div>
             
             <div class="faq-accordion">
                 <?php if(have_rows('faq')): ?>
