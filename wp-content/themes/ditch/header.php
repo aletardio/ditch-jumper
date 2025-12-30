9567004e2a89650c808ade7c2f09736d13cb24ed
aletardio <ale.tardio99@gmail.com>
2025-12-30 12:56:27 +0100

    fix menu

--- wp-content/themes/ditch/header.php
+++ wp-content/themes/ditch/header.php
@@ -37,12 +37,10 @@
 			<!-- Menu Mobile -->
 			<div class="site-header__col--nav">
                 <div class="mobile-menu-header">
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-header__logo">
-						<img style="filter:invert(1)"
-						src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/DITCH JUMPER.svg' ); ?>"
-						alt="Ditch Jumper"
-						>
-					</a>
+					<img style="filter:invert(1)"
+					src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/DITCH JUMPER.svg' ); ?>"
+					alt="Ditch Jumper"
+					>
                     <button class="mobile-menu-close" aria-label="Chiudi menu">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
