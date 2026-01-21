d4d1db2709b5644603b699a408baf8f71f4fe652
aletardio <ale.tardio99@gmail.com>
2026-01-21 11:20:38 +0100

    class update header & menu

--- wp-content/themes/ditch/header.php
+++ wp-content/themes/ditch/header.php
@@ -25,7 +25,7 @@
 <div id="page" class="site">
 	<header role="banner" id="masthead" class="site-header">
 		<div class="site-header__inner">
-			<div class="col-4 site-header__col site-header__col--logo">
+			<div class="col-4 site-header__col site-header__col--logo _70">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-header__logo">
 					<img
 					src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/DITCH JUMPER.svg' ); ?>"
@@ -37,13 +37,13 @@
 			<!-- Menu Tablet / Mobile -->
 			<div class="site-header__col--nav">
                 <div class="mobile-menu-header">
-					<div class="col-4 site-header__col site-header__col--logo">
+					<div class="_70">
 						<img style="filter:invert(1)"
 						src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/DITCH JUMPER.svg' ); ?>"
 						alt="Ditch Jumper"
 						>
 					</div>
-					<div class="col-4 site-header__col site-header__col--logo">
+					<div class="_30">
 						<button class="mobile-menu-close" aria-label="Chiudi menu">
 							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -64,7 +64,7 @@
 				</nav>
 			</div>
 
-			<div class="col-2 site-header__col site-header__col--cta">
+			<div class="col-2 site-header__col site-header__col--cta _30">
 				<button class="btn btn-primary text-xxs">Contattaci</button>
 				<!-- Hamburger Button per Mobile -->
 				<button class="mobile-menu-toggle" aria-label="Menu" aria-expanded="false" aria-controls="primary-menu">
@@ -76,4 +76,3 @@
 			<div class="mobile-menu-overlay"></div>
 		</div>
 	</header>
-
